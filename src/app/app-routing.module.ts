638fe9fc39162a87f7f1cc76b5a847514d59eedc
naveen-veera <veeranaveen.sethuraman@gmail.com>
2022-05-23 12:14:17 +0530

    initial-commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Component Routes
+import { JobListComponent } from './job-list/job-list.component'
+
+const routes: Routes = [
+  {path : '' , component: JobListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { JobListComponent } from './job-list/job-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    JobListComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
